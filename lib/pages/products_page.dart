79a1d3b0ac1addf1242a9cdf057733777bbffcc6
foyzur17 <foyzur17@gmail.com>
2022-09-30 23:29:21 +0600

    add push notification

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -33,6 +33,12 @@ class _ProductPageState extends State<ProductPage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
